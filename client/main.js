a044414d2f1cdc6bd65053c2244f0daff21c20c0
Jose Mendoza <jose@jjmendoza.com>
2017-11-26 09:22:06 -0800

    Setup mobile navigation

--- client/main.js
+++ client/main.js
@@ -14,13 +14,20 @@ Tracker.autorun(()=>{
 });
 
 Tracker.autorun(()=>{
-  const selectedNoteId = Session.get('selectedNoteId')
+  const selectedNoteId = Session.get('selectedNoteId');
+  Session.set('isNavOpen', false);
   if(selectedNoteId){
-    browserHistory.replace(`/dashboard/${selectedNoteId}`)
+    browserHistory.replace(`/dashboard/${selectedNoteId}`);
   }
 });
 
+Tracker.autorun(()=>{
+  const isNavOpen = Session.get('isNavOpen');
+  document.body.classList.toggle('is-nav-open', isNavOpen);
+})
+
 Meteor.startup(()=>{
   Session.set('selectedNoteId', undefined)
+  Session.set('isNavOpen', false);
   ReactDom.render(routes,  document.getElementById('app'));
 });
